f362b3fcd39d7970e769f7263fa30b78833704bd
Caitlin Pupich <pupich.caitlin@gmail.com>
2025-11-10 19:21:30 -0800

    Set react project and installed react-reouter-dom and taiwlindcss

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+ 
+  return(
+    <>
+    <Router>
+      <Routes>
+        <Route> </Route>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
