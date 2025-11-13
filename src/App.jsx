f9d4609506082aa9cf712567cd276a326f54b50b
Caitlin Pupich <pupich.caitlin@gmail.com>
2025-11-12 22:07:22 -0800

    Created the basic outline for the employee signup form and added a route to the app page for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import Login from './components/LoginForm/LoginForm'
+import LoginForm from './components/LoginForm/LoginForm.jsx'
+import SignUp from './components/SignUp/SignUp'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginForm/>} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </Router>
     </>
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,99 @@
+const SignUp = () => {
+    return(
+    //Full page container to control the spacing of the form using flexbox
+    <div class = "min-h-screen flex items-center justify-center p-4">
+        <div class="w-full max-w-sm">
+            <div class="text-center mb-6">
+                <h1 class="text-3xl font-extrabold text-gray-900 pb-2">Employee Signup</h1>
+                <p class="text-sm text-gray-700">Please create your employee profile with the form below</p>
+            </div>
+            
+            <form class = "bg-white p-8 border border-gray-200 rounded-xl shadow-2xl space-y-6 ">
+                    <div class = "space-y-2">
+                        <label 
+                            htmlFor="username"
+                            class = "block text-sm md:text-lg font-medium text-gray-700">
+                            Set Username:
+                        </label>
+                        <input 
+                            type="text" 
+                            id="username" 
+                            name="username" 
+                            required 
+                            class= "border rounded-lg shadow-sm p-1"
+                        />
+                    </div>
+
+                    <div class= "space-y-2">
+                        <label htmlFor="password"
+                            class = "block text-sm md:text-lg font-medium text-gray-700">
+                            Set Password:
+                        </label>
+                        <input 
+                            type="text"
+                            id="password"
+                            name="password"
+                            required 
+                            class= "border rounded-lg shadow-sm p-1"
+                        />
+                    </div>
+
+                    <div class= "space-y-2">
+                        <label htmlFor="password"
+                            class = "block text-sm md:text-lg font-medium text-gray-700">
+                            Full Name:
+                        </label>
+                        <input 
+                            type="text"
+                            id="password"
+                            name="password"
+                            required 
+                            class= "border rounded-lg shadow-sm p-1"
+                        />
+                    </div>
+
+
+                    <div class= "space-y-2">
+                        <label htmlFor="experience"
+                            class = "block text-sm md:text-lg font-medium text-gray-700">
+                            How many years of experience do you have?
+                        </label>
+                        <input 
+                            type="number"
+                            id="experience"
+                            name="experience"
+                            required 
+                            class= "border rounded-lg shadow-sm p-1"
+                        />
+                    </div>
+
+                <div class= "space-y-2">
+                        <label htmlFor="department"
+                        class = "block text-sm md:text-lg font-medium text-gray-700">
+                            What department do you work in?
+                        </label>
+                        <select
+                        id="experience"
+                        name="experience"
+                        required 
+                        class= "border rounded-lg shadow-sm p-1"
+                        >
+                            <option value="">Select a department</option>
+                            <option value="accounting">Accounting</option>
+                            <option value="engineering">Engineering</option>
+                            <option value="sales">Sales</option>
+                            <option value="marketing">Marketing</option>
+                        </select>
+                    </div>
+
+                <button 
+                    type="submit"
+                    class="border rounded-lg bg-indigo-600 text-white font-semibold px-4 py-2 w-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >Add Profile</button>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default SignUp;
