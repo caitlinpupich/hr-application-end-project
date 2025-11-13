800c6483d30cb8496f43d95ae80a2eac4388ef04
Caitlin Pupich <pupich.caitlin@gmail.com>
2025-11-12 20:40:17 -0800

    Added and formatted a form on the Login component and imported into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Login from './components/LogIn/LogIn'
 
 function App() {
  
@@ -9,7 +9,7 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route> </Route>
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
     </>
