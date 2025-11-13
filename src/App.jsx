a677d6f543f0371ed0e44781358f31c9464ccf51
Caitlin Pupich <pupich.caitlin@gmail.com>
2025-11-12 21:16:58 -0800

    Updating app page to include LoginForm name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import Login from './components/LogIn/LogIn'
+import Login from './components/LoginForm/LoginForm'
 
 function App() {
  
@@ -9,7 +9,7 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginForm/>} />
       </Routes>
     </Router>
     </>
